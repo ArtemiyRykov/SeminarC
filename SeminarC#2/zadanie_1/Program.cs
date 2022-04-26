7565d2fc49118bdcf5cbc467d42cf8f03790f15c
Artemiy <azazellorart@gmail.com>
2022-04-26 11:20:16 +0500

    zadanie_1

---
+++ SeminarC#2/zadanie_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int sum = Sum(20, 30);
+Print(Convert.ToString(sum));
+
+int Sum(int numberOne, int numberTwo) // функция суммы двух чисел. возвращаемая функция
+{
+    int sum = numberOne + numberTwo;
+    return sum;
+}
+
+void Print(string argument)          // функция вывод на печать. не возвращаемая функция
+{
+    Console.WriteLine(argument);
+}
