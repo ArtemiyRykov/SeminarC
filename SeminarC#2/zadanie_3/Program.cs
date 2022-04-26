7b8a69a3b842f1a1d0f6b50c2c3cd004b5198e20
Artemiy <azazellorart@gmail.com>
2022-04-26 16:36:06 +0500

    zadanie_3

---
+++ SeminarC#2/zadanie_3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*11. Напишите программу которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+Нужно использовать хотя бы одну функцию.
+456->46, 782->72, 918->98.*/
+
+int number = RandomInt(100,1000);
+Console.WriteLine($"Трех значное число: {number}");
+/*int number1 = number / 100;
+int number2 = number % 10;
+int result = number1 * 10 + number2;
+
+Console.WriteLine($"Трех значное число: {number}");
+Console.WriteLine($"Первая цифра: {number1}");
+Console.WriteLine($"Третья цифра: {number2}");
+Console.WriteLine($"Вывод числа без второй цифры: {result}");*/
+
+int result = ResultInt(number);
+
+Console.WriteLine($"Вывод числа без второй цифры: {result}");
+
+int ResultInt(int arg) // функция вычисления
+{
+    int first = arg / 100;
+    int second = arg % 10;
+    return first * 10 + second;
+}
+int RandomInt(int min, int max) // Функция рандома
+{
+    return new Random().Next(min, max);
+}
+
+//Console.WriteLine(Convert.ToString(number1) + Convert.ToString(nomber2)) // - Склейка двух чисел построчно
