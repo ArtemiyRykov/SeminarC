37d9489ed7d037c27edf14c028b6872480b91dc0
Artemiy <azazellorart@gmail.com>
2022-04-27 00:54:14 +0500

    zadanie_4

---
+++ SeminarC#3/zadanie_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// 22. Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратовчисел от 1 до N.
+// 5->1,4,9,16,25 .   2->1,4
+
+
+Console.Write("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+double index = 1;
+
+while (index <= number)
+{
+    double square = Math.Pow(index, 2);
+    index++;
+    Console.Write($"{square}, ");
+}
