3ad882e9089802abf3ef3de0d242c6750caf586c
Artemiy <azazellorart@gmail.com>
2022-04-28 01:31:03 +0500

    SeminarC#4_zadanie_2.2

--- SeminarC#4/zadanie_2/Program.cs
+++ SeminarC#4/zadanie_2/Program.cs
@@ -1,6 +1,8 @@
 ﻿// 26. Напите программу которая выдают принимает на вход число и выдает кол-во цифр чисел
 // 456->3,  78->2,  89126->5.
 
+//1 способ
+/*
 Console.Write("Введите число: ");
 string number = Convert.ToString(Console.ReadLine());
 
@@ -10,7 +12,7 @@ for (int i = 0; i < number.Length; i++)
     result++;
 }
 Console.WriteLine($"Ответ: {result}");
-
+*/
 
 // способ 2
 /*
@@ -19,3 +21,17 @@ string number = Convert.ToString(Console.ReadLine());
 int sqr = number.Length;
 Console.WriteLine($"Ответ: {sqr}");
 */
+
+// 3 способ
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+while (number != 0)
+{
+    number = number / 10;
+
+    index++;
+}
+Console.WriteLine($"Ответ: {index}");
