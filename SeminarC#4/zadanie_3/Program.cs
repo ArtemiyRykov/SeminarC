a2b83d7b04bffa7bdb4e503609b70a59a996a332
Artemiy <azazellorart@gmail.com>
2022-04-28 11:46:55 +0500

    SeminarC#4\zadanie_3

---
+++ SeminarC#4/zadanie_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// 28. Напишите программу, которая принимает на вход число от 1 до number
+// 4->24, 5->120.
+
+
+// while Способ 1
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+int faсt = 1;
+while (index <= number)
+{
+    faсt = index * faсt;
+    index++;
+}
+Console.Write("Ответ: " + faсt);
+*/
+
+//for способ 1
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int faсt = 1;
+for (int i = 1; i <= number; i++) faсt = i * faсt;
+
+Console.Write("Ответ: " + faсt);
