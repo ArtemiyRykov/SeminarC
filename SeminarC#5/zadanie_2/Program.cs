b6c7ffb4350ce886a17252c478bbe3d8d9de785d
Artemiy <azazellorart@gmail.com>
2022-05-02 17:17:49 +0500

    SeminarC#5\zadanie_2

---
+++ SeminarC#5/zadanie_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 32: Напишите программу замена элементов 
+// массива: положительные элементы замените на 
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+int[] numbers = new int[4];
+for (int i = 0; i < numbers.Length; i++) // цикл для ввода чисел
+{
+    Console.Write($"Введите элемент массива {i + 1}: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+WriteArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++) numbers[i] *= -1;
+
+WriteArray(numbers);
+
+
+
+
+
+void WriteArray(int[] array)  // функция вывода массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
