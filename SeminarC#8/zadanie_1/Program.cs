6024bef973ac26fa785f06c28edf9920c6ae5f05
Artemiy <azazellorart@gmail.com>
2022-05-17 00:27:55 +0500

    SeminarC#8\zadanie_1

---
+++ SeminarC#8/zadanie_1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет 
+// местами первую и последнюю строку массива.
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int o
+int[,] array = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    int temp = array[0, i];
+    array[0, i] = array[m - 1, i];
+    array[m - 1, i] = temp;
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
