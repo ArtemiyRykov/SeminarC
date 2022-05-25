00e770c4dacbeafa7ba93e0b9e9ddf033f5fb63f
Artemiy <azazellorart@gmail.com>
2022-05-25 23:49:10 +0500

    SeminarC#9\zadanie_3\

---
+++ SeminarC#9/zadanie_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Numbers(number);
+Console.WriteLine(result);
+
+int Numbers(int number)
+{
+    if (number > 0)
+    {
+        return number%10 + Numbers(number/10);
+    }
+    return 0;
+}
