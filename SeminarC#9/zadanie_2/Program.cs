f08c957c34187307187b690a08a5fe293f5d8701
Artemiy <azazellorart@gmail.com>
2022-05-25 23:13:20 +0500

    SeminarC#9\zadanie_2\

---
+++ SeminarC#9/zadanie_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"*/
+
+Console.WriteLine("Задайте число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Rekurs(m,n));
+
+int Rekurs(int numStart, int numEnd)
+{
+    if(numEnd == numStart)
+    {
+        return numEnd;
+    }
+    Console.Write(Rekurs(numStart,numEnd - 1) + ", ");
+    return numEnd;
+}
+
