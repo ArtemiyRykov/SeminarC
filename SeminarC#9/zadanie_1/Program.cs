030625096a6449d15c5acb50b6ffe97a8c576d99
Artemiy <azazellorart@gmail.com>
2022-05-25 23:12:54 +0500

    SeminarC#9\zadanie_1\

---
+++ SeminarC#9/zadanie_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 63: Задайте значени N. Напишите программу, которая 
+// выведет все натуральный числа в промежутки от 1 до N.
+// Решить с помощью рекурсии.
+
+Console.WriteLine("Задайте число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Rekurs(n);
+Console.Write(result);
+
+int Rekurs(int number)
+{
+    if(number == 1)
+    {
+        return 1;
+    }
+    int temp = Rekurs(number - 1);
+    Console.Write(temp + ", ");
+    return number;
+}
